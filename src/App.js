accfd603160421721fce064d13e8dca3b036c3f0
blobquiet <soquiet@hotmail.com>
2022-10-30 18:34:36 +0100

    update

--- src/App.js
+++ src/App.js
@@ -4,7 +4,6 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import { Image, Row, Col, Container } from "react-bootstrap";
 
 function App() {
-  
   const [displayNone, setDisplayNone] = useState("");
   const [fadeClassOut, setFadeOut] = useState("");
   const [copySuccess, setCopySuccsess] = useState("Code");
@@ -14,18 +13,22 @@ function App() {
   const [backgroundColor, setBackgroundColor] = useState();
   /* var bgColor= ["rgb(116, 128, 233)",
      "rgb(116, 120, 233)", "rgb(116, 140, 233)", "rgb(116, 110, 233)","rgb(116, 140, 233)"]; */
-    var bgColor= ["rgb(116, 128, 233)",
-     "rgb(68, 184, 64)", "rgb(255, 133, 33)",
-    "rgb(255 82 82)", "rgb(73, 178, 209)"];
+  var bgColor = [
+    "rgb(116, 128, 233)",
+    "rgb(68, 184, 64)",
+    "rgb(255, 133, 33)",
+    "rgb(255 82 82)",
+    "rgb(73, 178, 209)",
+  ];
 
-    const copyToClipboard = (e) => {
-      /* textAreaRef.current.select(); */
-      /* document.execCommand('copy'); */
-      // This is just personal preference.
-      // I prefer to not show the whole text area selected.
-      //e.target.focus();
-      setCopySuccsess("Send us an email :)");
-    };
+  const copyToClipboard = (e) => {
+    /* textAreaRef.current.select(); */
+    /* document.execCommand('copy'); */
+    // This is just personal preference.
+    // I prefer to not show the whole text area selected.
+    //e.target.focus();
+    setCopySuccsess("Send us an email :)");
+  };
 
   useEffect(() => {
     setTimeout(() => {
@@ -35,25 +38,29 @@ function App() {
       setDisplayNone("displayNone");
     }, 4000);
     setBackgroundColor(bgColor[Math.floor(Math.random() * bgColor.length)]);
-  }, [])
+  }, []);
 
   return (
     <>
-   {/* <div id="counter-app">
+      {/* <div id="counter-app">
      <div id="buttons-container" className={`container ${fadeClassIn}`}>
       <button id="increment-button" className="button" ><i className="fa fa-plus"></i></button>
       <button id="decrement-button" className="button" ><i className="fa fa-minus"></i></button>
       <button id="reset-button" className="button" ><i className="fa fa-refresh"></i></button>
     </div> 
   </div>*/}
-      <div className="App App-header" style={{backgroundColor:backgroundColor}}>
-        <h1          
+      <div
+        className="App App-header"
+        style={{ backgroundColor: backgroundColor }}
+      >
+        <h1
           className={`animate__animated animate__bounce ${fadeClassOut} ${displayNone}`}
           style={{ fontSize: "2.5em", position: "absolute" }}
         >
-          First place in 2019 SIIM-ISIC skin lesion challenge
+          First place in 2019
+          <br /> SIIM-ISIC skin lesion challenge
         </h1>
-        <p        
+        <p
           className={`animate__animated animate__delay-4s animate__fadeIn `}
           style={{
             fontSize: "1.1em",
@@ -64,48 +71,81 @@ function App() {
             paddingRight: "0.5em",
           }}
         >
-          
           <Container>
-          <h2 style={{marginBottom:"1em"}}>Using Neural Networks to discriminate between Benign and Malignant Moles
-          </h2>
-          <Row>
-            <Col>
-            <a style={{color: "white"}} href="Using CNNs to discriminate between benign and malignant moles.pdf" download target="_blank">
-              <Image width="93" src="paper_image.png" thumbnail/>
-              <p style={{
-                position:"relative",
-                fontSize: "0.8em",
-                fontWeight: "500",
-                textAlign: "center",
-                marginTop: "0.5em",
-                fontWeight: "1500"
-                }}>Paper</p></a>
-            </Col>
-            <Col>
-            <a style={{color: "white"}} href="skin-moles.apk" download target="_blank">
-              <Image width="140" src="apk3.png"/>
-              <p style={{
-                position:"relative",
-                fontSize: "0.8em",
-                fontWeight: "500",
-                textAlign: "center",
-                marginTop: "0.5em",
-                fontWeight: "1500",
-                }}>skin-moles.apk</p></a>
-            </Col>
-            <Col>
-            <a style={{color: "white"}} href="https://github.com/mbirkholzupc/mai_ci_ddmr" target="_blank">
-              <Image width="140" src="git.png"/>
-              <p style={{
-                position:"relative",
-                fontSize: "0.8em",
-                fontWeight: "500",
-                textAlign: "center",
-                marginTop: "0.5em",
-                fontWeight: "1500",
-                }}>Code</p></a>
-            </Col>     
-            {/*<Col>
+            <h2 style={{ marginBottom: "1em" }}>
+              Using Neural Networks to discriminate between Benign and Malignant
+              Moles
+            </h2>
+            <Row>
+              <Col>
+                <a
+                  style={{ color: "white" }}
+                  href="_David_D__Gaviria__Superconverging_Deep_Learning_Model_for_Skin_Lesion_Classification.pdf"
+                  without
+                  rel="noopener noreferrer"
+                  target="_blank"
+                >
+                  <Image width="93" src="paper_image.png" thumbnail />
+                  <p
+                    style={{
+                      position: "relative",
+                      fontSize: "0.8em",
+                      fontWeight: "500",
+                      textAlign: "center",
+                      marginTop: "0.5em",
+                      fontWeight: "1500",
+                    }}
+                  >
+                    Paper
+                  </p>
+                </a>
+              </Col>
+              <Col>
+                <a
+                  style={{ color: "white" }}
+                  href="skin-moles.apk"
+                  download
+                  target="_blank"
+                >
+                  <Image width="140" src="skin-icon.png" />
+                  <p
+                    style={{
+                      position: "relative",
+                      fontSize: "0.8em",
+                      fontWeight: "500",
+                      textAlign: "center",
+                      marginTop: "0.5em",
+                      fontWeight: "1500",
+                    }}
+                  >
+                    Skin Classification
+                    <br />
+                    Web App
+                  </p>
+                </a>
+              </Col>
+              <Col>
+                <a
+                  style={{ color: "white" }}
+                  href="https://github.com/blobquiet/SIIM-ISIC-Melanoma-Classification"
+                  target="_blank"
+                >
+                  <Image width="140" src="git.png" />
+                  <p
+                    style={{
+                      position: "relative",
+                      fontSize: "0.8em",
+                      fontWeight: "500",
+                      textAlign: "center",
+                      marginTop: "0.5em",
+                      fontWeight: "1500",
+                    }}
+                  >
+                    Code
+                  </p>
+                </a>
+              </Col>
+              {/*<Col>
              <div onClick={() => copyToClipboard()}>
               <Image width="93" src="git.png"/></div>
               <p style={{
@@ -118,127 +158,126 @@ function App() {
                 }}> {copySuccess}</p> 
                 
             </Col>*/}
-          </Row>
-        </Container>      
+            </Row>
+          </Container>
         </p>
       </div>
 
       <Container className="animate__animated animate__delay-2s animate__fadeIn">
         <Row>
           <Col>
-          <div>
-        <h3
-          style={{
-            fontSize: "1em",
-            fontWeight: "800",
-            textAlign: "center",
-            marginTop: "0.8em",
-          }}
-        >
-          Petia Ivanona Radeva
-        </h3>
-        <p
-          style={{
-            fontSize: "0.7em",
-            fontWeight: "500",
-            textAlign: "center",
-            marginTop: "0.5em",
-            paddingLeft: "0.5em",
-            paddingRight: "0.5em",
-          }}
-        >
-          Universitat de Barcelona
-        </p>
-      </div>
-      <p
-        className="animate__animated animate__delay-4s animate__fadeIn"
-        style={{
-          fontSize: "0.7em",
-          fontWeight: "20",
-          textAlign: "center",
-          color: "black",
-        }}
-      >
-        radevap@gmail.com
-      </p>
+            <div>
+              <h3
+                style={{
+                  fontSize: "1em",
+                  fontWeight: "800",
+                  textAlign: "center",
+                  marginTop: "0.8em",
+                }}
+              >
+                Petia Ivanona Radeva
+              </h3>
+              <p
+                style={{
+                  fontSize: "0.7em",
+                  fontWeight: "500",
+                  textAlign: "center",
+                  marginTop: "0.5em",
+                  paddingLeft: "0.5em",
+                  paddingRight: "0.5em",
+                }}
+              >
+                Universitat de Barcelona
+              </p>
+            </div>
+            <p
+              className="animate__animated animate__delay-4s animate__fadeIn"
+              style={{
+                fontSize: "0.7em",
+                fontWeight: "20",
+                textAlign: "center",
+                color: "black",
+              }}
+            >
+              radevap@gmail.com
+            </p>
           </Col>
           <Col>
-          <div>
-        <h3
-          style={{
-            fontSize: "1em",
-            fontWeight: "800",
-            textAlign: "center",
-            marginTop: "0.8em",
-          }}
-        >
-          David Dueñas
-        </h3>
-        <p
-          style={{
-            fontSize: "0.7em",
-            fontWeight: "500",
-            textAlign: "center",
-            marginTop: "0.5em",
-            paddingLeft: "0.5em",
-            paddingRight: "0.5em",
-          }}
-        >
-          Universitat Politècnica de Catalunya
-        </p>
-      </div>
-      <p
-        className="animate__animated animate__delay-4s animate__fadeIn"
-        style={{
-          fontSize: "0.7em",
-          fontWeight: "20",
-          textAlign: "center",
-          color: "black",
-        }}
-      >
-        blobquiet@gmail.com
-      </p>
+            <div>
+              <h3
+                style={{
+                  fontSize: "1em",
+                  fontWeight: "800",
+                  textAlign: "center",
+                  marginTop: "0.8em",
+                }}
+              >
+                David Dueñas
+              </h3>
+              <p
+                style={{
+                  fontSize: "0.7em",
+                  fontWeight: "500",
+                  textAlign: "center",
+                  marginTop: "0.5em",
+                  paddingLeft: "0.5em",
+                  paddingRight: "0.5em",
+                }}
+              >
+                Universitat Politècnica de Catalunya
+              </p>
+            </div>
+            <p
+              className="animate__animated animate__delay-4s animate__fadeIn"
+              style={{
+                fontSize: "0.7em",
+                fontWeight: "20",
+                textAlign: "center",
+                color: "black",
+              }}
+            >
+              blobquiet@gmail.com
+            </p>
           </Col>
           <Col>
-          <div>
-        <h3
-          style={{
-            fontSize: "1em",
-            fontWeight: "800",
-            textAlign: "center",
-            marginTop: "0.8em",
-          }}
-        >
-          Md Mostafa Kamal Saker
-        </h3>
-        <p
-          style={{
-            fontSize: "0.7em",
-            fontWeight: "500",
-            textAlign: "center",
-            marginTop: "0.5em",
-            paddingLeft: "0.5em",
-            paddingRight: "0.5em",
-          }}
-        >
-          University of Oxford
-        </p>
-      </div>
-      <p
-        className="animate__animated animate__delay-4s animate__fadeIn"
-        style={{
-          fontSize: "0.7em",
-          fontWeight: "20",
-          textAlign: "center",
-          color: "black",
-        }}
-      >
-        m.kamal.sarker@gmail.com
-      </p>
+            <div>
+              <h3
+                style={{
+                  fontSize: "1em",
+                  fontWeight: "800",
+                  textAlign: "center",
+                  marginTop: "0.8em",
+                }}
+              >
+                Md Mostafa Kamal Saker
+              </h3>
+              <p
+                style={{
+                  fontSize: "0.7em",
+                  fontWeight: "500",
+                  textAlign: "center",
+                  marginTop: "0.5em",
+                  paddingLeft: "0.5em",
+                  paddingRight: "0.5em",
+                }}
+              >
+                University of Oxford
+              </p>
+            </div>
+            <p
+              className="animate__animated animate__delay-4s animate__fadeIn"
+              style={{
+                fontSize: "0.7em",
+                fontWeight: "20",
+                textAlign: "center",
+                color: "black",
+              }}
+            >
+              m.kamal.sarker@gmail.com
+            </p>
           </Col>
         </Row>
       </Container>
-      
     </>
   );
 }
