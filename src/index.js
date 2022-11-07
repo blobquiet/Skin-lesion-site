96d17fff640a75adcf437e2dd3fc91215aced061
blobquiet <soquiet@hotmail.com>
2022-11-07 22:47:22 +0100

    adding new favicon

--- src/index.js
+++ src/index.js
@@ -1,34 +1,35 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import React from "react";
+import ReactDOM from "react-dom";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import './index.css';
-import App from './App';
-import Classification from './components/Classification/Classification';
-import Prediction from './components/Prediction/Prediction';
-import reportWebVitals from './reportWebVitals';
-
+import "./index.css";
+import App from "./App";
+import Classification from "./components/Classification/Classification";
+import Prediction from "./components/Prediction/Prediction";
+import reportWebVitals from "./reportWebVitals";
 
 ReactDOM.render(
   <React.StrictMode>
-       <Router>
+    <Router>
       <Routes>
-        <Route exact path="/" element={<App/>} />
-        <Route path="/home" element={<App/>} />
-        <Route path="/classification" element={<Classification/>} />
-        <Route path="/prediction" element={<Prediction/>} />
+        <Route exact path="/" element={<App />} />
+        <Route path="/home" element={<App />} />
+        <Route path="/classification" element={<Classification />} />
+        <Route path="/prediction" element={<Prediction />} />
       </Routes>
     </Router>
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
-{/*
+{
+  /*
 ReactDOM.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
   document.getElementById('root')
 );
-*/}
+*/
+}
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
